d5080b767a68f0c9f3f4274097e576641cf78e03
Milan G <dev@milangress.de>
2022-07-29 12:43:19 +0200

    basic api

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
-<template>
-  <Tutorial />
+<template lang="pug">
+  div
+    p test
+    div(v-for="date in dates") {{date.name}}
+      img(
+      :src="getImage(date.image)"
+      )
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data () {
+    return {
+      baseUrl: 'https://rypqx7qi.directus.app/',
+      dates: []
+    }
+  },
+  async fetch() {
+    const data = await fetch(this.baseUrl + 'items/Dates').then((response) => response.json())
+    this.dates = await data.data
+    // eslint-disable-next-line no-console
+    console.log(this.dates)
+  },
+  methods: {
+    getImage (fileId) {
+      const url = new URL(`${this.baseUrl}assets/${fileId}`)
+      return url.href
+    }
+  },
 }
 </script>
