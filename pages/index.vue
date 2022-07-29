f6fc46abb1db31054c53ab5d1401023575a933d5
Milan G <dev@milangress.de>
2022-07-29 19:34:29 +0200

    about text

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,9 @@
     headline-dynamic
       h1 Es geht weiter und wir legen wieder los!
     timetable-block(:allDates="dates")
-    headline-dynamic
-      h1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+    .content.inner(v-html="about.about" )
+
     //nuxt-img(src="/directus/assets/caf65d7b-0788-42c8-8bfd-77c48535f572" provider="static")
     div(v-for="date in dates")
       headline-dynamic
@@ -31,14 +32,20 @@ export default {
   data () {
     return {
       baseUrl: 'https://rypqx7qi.directus.app/',
-      dates: []
+      dates: [],
+      about: []
     }
   },
   async fetch() {
-    const data = await fetch(this.baseUrl + 'items/Dates').then((response) => response.json())
-    this.dates = await data.data
+    const dataDates = await fetch(this.baseUrl + 'items/Dates').then((response) => response.json())
+    this.dates = await dataDates.data
     // eslint-disable-next-line no-console
     console.log(this.dates)
+
+    const dataAbout = await fetch(this.baseUrl + 'items/about').then((response) => response.json())
+    this.about = await dataAbout.data
+    // eslint-disable-next-line no-console
+    console.log(this.about)
   },
   methods: {
     getImage (fileId) {
@@ -51,7 +58,7 @@ export default {
 <style>
 :root {
   --width-outer: 90vw;
-  --width-inner: calc(var(--width-outer) - 10rem);
+  --width-inner: calc(var(--width-outer) - 20rem);
   --pink: #F9D1E4;
   --green: #37AD60;
   --yellow: #E4DF00;
@@ -60,5 +67,19 @@ export default {
   width: var(--width-outer);
   margin: 0 auto;
   display: block;
+}
+.inner {
+  width: var(--width-inner);
+  margin: 0 auto;
+}
+.outer {
+  width: var(--width-outer);
+  margin: 0 auto;
+}
+.content {
+  background-color: var(--pink);
+  margin-block: 3rem;
+  padding: 0.5rem;
+  font-size: 1.2rem;
 }
 </style>
