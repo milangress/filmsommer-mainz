8629f628b52319182e12fcbd39dc41a0c2a1ad93
Milan G <dev@milangress.de>
2022-07-29 17:45:29 +0200

    date & styling

--- components/TimetableBlock.vue
+++ components/TimetableBlock.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
 .timetable
   .day(v-for="date in allDates")
-    h2 {{date.date}}
+    h2 {{getDate(date.date)}}
     .event(v-for="event in date.events")
-      p {{event.time}} {{event.type}}
+      strong {{getTime(event.time)}} {{event.type}}
       p {{event.title}}
 
 </template>
 
 <script>
+const { DateTime } = require("luxon");
+
 export default {
   name: "TimetableBlock",
   props: {
@@ -17,17 +19,33 @@ export default {
       required: true,
     }
   },
+  methods: {
+    getTime(time) {
+      return DateTime.fromISO(time).toFormat("HH:mm");
+    },
+    getDate(date) {
+      const weekday = DateTime.fromISO(date).weekdayShort
+        // .toFormat("dd.MM.yyyy");
+      const day = DateTime.fromISO(date).toFormat("dd.MM")
+      return `${weekday}, ${day}`
+    }
+  }
 }
 </script>
 
 <style scoped>
 .timetable {
+  padding: 0.5rem;
   width: var(--width-outer);
   margin: 0 auto;
   background-color: var(--pink);
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   grid-auto-rows: auto;
+  grid-gap: 2rem;
+}
+.event {
+  min-height: 5em
 }
 
 </style>
