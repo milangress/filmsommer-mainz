4405e1698f40f5576842208fee59a14053ed6256
Milan G <dev@milangress.de>
2022-07-31 14:49:02 +0200

    pathtext stroke 1px

--- components/PathText.vue
+++ components/PathText.vue
@@ -1,6 +1,6 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="391.108" height="147.962" viewBox="0 0 400 160">
-    <path id="curve" d="M.13,113.538s76.3,46.339,169.446,30.993S356.609,67.017,390.886.114" transform="translate(0 0)" fill="none" :stroke="color" stroke-width="0.5"/>
+    <path id="curve" d="M.13,113.538s76.3,46.339,169.446,30.993S356.609,67.017,390.886.114" transform="translate(0 0)" fill="none" :stroke="color" stroke-width="1"/>
     <text :fill="color">
       <textPath alignment-baseline="top" xlink:href="#curve">
         {{text}}
