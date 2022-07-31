089113d0229e446a227ee80149aa9c0b5afb84e5
Milan G <dev@milangress.de>
2022-07-31 17:36:44 +0200

    add impressum link

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,7 @@
     //nuxt-img(src="/directus/assets/caf65d7b-0788-42c8-8bfd-77c48535f572" provider="static")
     div(v-for="date in dates")
       nuxt-img.date-image(
+        format="png"
         sizes="sm:100vw md:50vw lg:1900px"
         provider="static"
         :src="getImage(date.image)"
@@ -35,6 +36,10 @@
       //provider="static"
       //:src="getImage(date.image)"
       //)
+    .content.inner
+      nuxt-link(
+        to="/impressum"
+        ) Impressum
 </template>
 
 <script>
