0e4bfb40c4db96706f8f32584dfb24ea774c7fd3
Milan G <dev@milangress.de>
2023-06-27 17:18:27 +0200

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,23 @@
 <script>
 	import BackgroundDynamic from '../components/BackgroundDynamic.svelte';
+	import { VERCEL_ENV } from '$env/static/private';
+
+	function noIndexOnVercel() {
+		if (
+			VERCEL_ENV === 'preview' || VERCEL_ENV === 'development'
+		) {
+			return 'noindex'
+		} else {
+			return  'index, follow'
+		}
+	}
 </script>
 
+
+<svelte:head>
+	<meta name="robots" content="{noIndexOnVercel()}">
+	{noIndexOnVercel()} {VERCEL_ENV}
+</svelte:head>
 <div>
 	<BackgroundDynamic />
 	<slot />
