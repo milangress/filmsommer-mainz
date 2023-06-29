7777d7a5f1c24c176e2e71b75c43c0fe2465212a
Milan G <dev@milangress.de>
2023-06-29 12:51:52 +0200

    add og data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 
 
 <svelte:head>
+	<title>Filmsommer 2023 – 10. FILMSOMMER FESTIVAL MAINZ</title>
+	<meta name="description" content="Der Filmsommer Mainz ist die beste Open-Air-Kinoreihe im Rhein-Main-Gebiet. Umsonst und unter freiem Himmel.">
+	<meta property="og:image" content="/FilmsommerLogo.svg">
 	<meta name="robots" content="{data.noIndexOnVercel}">
 </svelte:head>
 <div>
