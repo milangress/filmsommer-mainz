dd57c66c104e9ec6f3226fe14fe0c99bd727b9f3
Milan G <dev@milangress.de>
2023-07-18 15:43:43 +0200

    images

--- src/components/LogoBlock.svelte
+++ src/components/LogoBlock.svelte
@@ -1,19 +1,22 @@
 <script>
 	let logos = []
 
-	async function fetchLogos() {
+	/*async function fetchLogos() {
 		logos = Array.from(Array(23).keys()).map((i) => ({
 			file: `sponsor_logo_${i}`
 		}))
 	}
 
-	fetchLogos()
+	fetchLogos()*/
 </script>
 
-<div class="logos">
+<!--<div class="logos">
 	{#each logos as logo}
 		<img src={`logos/${logo.file}.svg`} loading="lazy" alt="Sponsor Logo" class="sponsor" />
 	{/each}
+</div>-->
+<div class="logoblock">
+	<img src="logos.png" alt="logos">
 </div>
 
 <style>
