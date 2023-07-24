dcc96798e7c77224615473958d7f982241055bcb
Milan G <dev@milangress.de>
2023-07-24 15:03:56 +0200

    hide headline

--- src/components/TimetableBlock.svelte
+++ src/components/TimetableBlock.svelte
@@ -134,4 +134,12 @@
 	.event {
 		min-height: 5em;
 	}
+	.visually-hidden {
+		position: absolute;
+		left:     -10000px;
+		top:      auto;
+		width:    1px;
+		height:   1px;
+		overflow: hidden;
+	}
 </style>
