fa39ee7459789f38da1b649db22495898d45688a
Milan G <dev@milangress.de>
2023-07-27 11:37:25 +0200

    inline more styles

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,8 @@ import adapter from '@sveltejs/adapter-static'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// inline all stylesheets smaller than 1kb
-		inlineStyleThreshold: 1024,
+		// inline all stylesheets smaller than 2kb
+		inlineStyleThreshold: 2048,
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
